ce6e832736a730fbc29375650d08e2f9ac35814b
KronCyrcul <ibloko1504@gmail.com>
2018-08-15 19:29:56 +0300

    Simplified output_courses_into_to_xlsx function

--- coursera.py
+++ coursera.py
@@ -5,18 +5,17 @@ import os
 import re
 
 
-def get_courses_list(url, courses_count, page_start, courses_links):
-    params = {"start": page_start}
+def get_courses_list(url, courses_count, page_search, courses_links):
+    params = {"start": page_search}
     response = requests.get(url)
     response_soup = BeautifulSoup(response.text, "html.parser")
     research_result = response_soup.find_all(
         "div",
         {"data-courselenium": "catalog_search_result"})
-    all_links = research_result[0].find_all("a", href=True)
-    courses_links += [link["href"] for link in all_links if
-        re.search(r"/learn/", link["href"])]
+    courses_links += [link["href"] for link in research_result[0].find_all(
+        "a", href=re.compile(r"/learn/"))]
     if len(courses_links) <= courses_count:
-        page_start += 20
+        page_search += 20
         get_courses_list(url, courses_count, page_start, courses_links)
     return courses_links
 
@@ -41,31 +40,29 @@ def get_courses_info(url, course_link):
     return course_info
 
 
-def output_courses_info_to_xlsx(filepath, courses_info):
-    workbook = Workbook()
-    worksheet = workbook.active
-    course_row = 2
-    for course in courses_info:
+def output_courses_info_to_xlsx(workbook, course_info, course_row):
+    for key, value in course_info.items():
         try:
             first_row = worksheet.rows[0]
+            first_row_values = [cell.value for cell in first_row]
+            worksheet.cell(
+                row=course_row,
+                column=first_row_values.index(key)+1).value = value
         except IndexError:
-            worksheet.append(list(course.keys()))
+            worksheet.append(list(course_info.keys()))
             first_row = worksheet.rows[0]
-        first_row_values = [cell.value for cell in first_row]
-        for key, value in course.items():
-            if key not in first_row_values:
-                worksheet.cell(
-                    row=1,
-                    column=len(first_row_values)+1).value = key
-                worksheet.cell(
-                    row=course_row,
-                    column=len(first_row_values)+1).value = value
-            else:
-                worksheet.cell(
-                    row=course_row,
-                    column=first_row_values.index(key)+1).value = value
-        course_row += 1
-    workbook.save("{}/{}".format(filepath, "courses.xlsx"))
+            first_row_values = [cell.value for cell in first_row]
+            worksheet.cell(
+                row=course_row,
+                column=first_row_values.index(key)+1).value = value
+        except ValueError:
+            worksheet.cell(
+                row=1,
+                column=len(first_row_values)+1).value = key
+            worksheet.cell(
+                row=course_row,
+                column=len(first_row_values)+1).value = value
+    return workbook
 
 
 if __name__ == "__main__":
@@ -74,8 +71,19 @@ if __name__ == "__main__":
     courses_url = "{}/{}".format(url, "courses")
     courses_count = 20
     page_start = 0
-    courses_links = get_courses_list(courses_url, courses_count, page_start, list())
-    courses_info = []
+    courses_links = get_courses_list(
+        courses_url,
+        courses_count,
+        page_start,
+        list())
+    workbook = Workbook()
+    worksheet = workbook.active
+    course_row = 2
     for course in courses_links:
-        courses_info.append(get_courses_info(url, course))
-    output_courses_info_to_xlsx(filepath, courses_info)
+        course_info = get_courses_info(url, course)
+        workbook = output_courses_info_to_xlsx(
+            workbook,
+            course_info,
+            course_row)
+        course_row += 1
+    workbook.save("{}/{}".format(filepath, "courses.xlsx"))
